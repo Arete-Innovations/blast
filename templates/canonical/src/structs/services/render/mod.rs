c103b7fa43352d0abb0313344fcf50b2e87cc4e1
ZmoleCristian <tragdate@gmail.com>
2026-05-01 14:22:40 +0300

    services/render: cross-target render module + ListBuilder

--- templates/canonical/src/structs/services/render/mod.rs
+++ templates/canonical/src/structs/services/render/mod.rs
@@ -1,5 +1,7 @@
 pub mod form;
+pub mod list;
 pub mod table;
 
 pub use form::{FieldMeta, FormBuilder, FormPlanEntry, FormSubmitFn, InputKind};
+pub use list::{ListBuilder, ListItemTemplate, ListType};
 pub use table::{Formatter, TableBuilder, TableRenderClasses};
